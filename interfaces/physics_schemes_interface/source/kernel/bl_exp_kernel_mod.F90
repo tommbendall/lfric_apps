febe2770a62922606c49f9f2c949d54ffd78d5bb
Thomas Bendall <14180399+tommbendall@users.noreply.github.com>
2026-06-28 21:24:27 +0100

    further fixes to energy correction and BL

--- interfaces/physics_schemes_interface/source/kernel/bl_exp_kernel_mod.F90
+++ interfaces/physics_schemes_interface/source/kernel/bl_exp_kernel_mod.F90
@@ -384,8 +384,7 @@ contains
     use nlsizes_namelist_mod, only: bl_levels
     use planet_constants_mod, only: p_zero, kappa, planet_radius, cp_bl
     use water_constants_mod,  only: lc, lf, tm
-    use bl_cpm_mod,           only: cpv_cpm_bl, cl_cpm_bl, ci_cpm_bl,            &
-                                    bl_mload_switch
+    use bl_cpm_mod,           only: cpv_cpm_bl, cl_cpm_bl, ci_cpm_bl
     use timestep_mod, only: timestep
 
     use free_tracers_inputs_mod,    only: n_wtrac
@@ -805,8 +804,7 @@ contains
         tl(i,1,k) = (cpm/cpm_dag)*temperature(i,1,k)                           &
                   - (lrv0/cpm_dag)*qcl(i,1,k)                                  &
                   - (lrs0/cpm_dag)*qcf(i,1,k)
-        qw(i,1,k) = q(i,1,k) + qcl(i,1,k) + qcf(i,1,k)                         &
-                  + bl_mload_switch*(qrain(i,1,k) + qgraupel(i,1,k))
+        qw(i,1,k) = q(i,1,k) + qcl(i,1,k) + qcf(i,1,k)
       end do
     end do
 
