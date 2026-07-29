4f81bd234377906b60c0902ecb92ed435d6b3f71
Thomas Bendall <14180399+tommbendall@users.noreply.github.com>
2026-07-29 09:37:36 +0100

    deploy upgrade macro

--- applications/linear_model/rose-meta/lfric-linear_model/versions.py
+++ applications/linear_model/rose-meta/lfric-linear_model/versions.py
@@ -31,3 +31,50 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+
+class vn32_t670(MacroUpgrade):
+    """Upgrade macro for ticket #670 by Thomas Bendall."""
+
+    BEFORE_TAG = "vn3.2"
+    AFTER_TAG = "vn3.2_t670"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-gungho
+        # Add new nudging namelist options
+        self.add_setting(
+            config, ["namelist:nudging", "nudging_method"], "'convolution'"
+        )
+        self.add_setting(
+            config, ["namelist:nudging", "nudging_relax_time"], "3600.0"
+        )
+        self.add_setting(
+            config, ["namelist:nudging", "nudging_spinup_start"], "432000.0"
+        )
+        self.add_setting(
+            config, ["namelist:nudging", "nudging_spinup_end"], "864000.0"
+        )
+        self.add_setting(config, ["namelist:nudging", "spectral_kmax"], "20")
+        self.add_setting(config, ["namelist:nudging", "spectral_kmin"], "2")
+        self.add_setting(
+            config, ["namelist:nudging", "spectral_stencil_extent"], "12"
+        )
+        # Remove retired setting
+        self.remove_setting(config, ["namelist:nudging", "nudging_source"])
+        # If nudging_mesh_name is still the default '' value, set it to
+        # match dynamics_mesh_name
+        nudging_mesh_name = self.get_setting_value(
+            config, ["namelist:multires_coupling", "nudging_mesh_name"]
+        )
+        if nudging_mesh_name == "''":
+            dynamics_mesh_name = self.get_setting_value(
+                config, ["namelist:multires_coupling", "dynamics_mesh_name"]
+            )
+            if dynamics_mesh_name is not None:
+                self.change_setting_value(
+                    config,
+                    ["namelist:multires_coupling", "nudging_mesh_name"],
+                    dynamics_mesh_name,
+                )
+
+        return config, self.reports
