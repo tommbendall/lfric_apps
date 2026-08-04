bacd3d210d76755b78e52bbbff056be9048aef33
Thomas Bendall <14180399+tommbendall@users.noreply.github.com>
2026-08-04 11:56:24 +0100

    working settings

--- science/gungho/source/configuration/check_configuration_mod.F90
+++ science/gungho/source/configuration/check_configuration_mod.F90
@@ -774,7 +774,7 @@ contains
 
       if ( use_spectral_nudging                                                &
            .and. trim(base_mesh_names(i)) == trim(nudging_mesh_name) ) then
-        stencil_depths(i) = MAX(stencil_depths(i), spectral_stencil_extent)
+        stencil_depths(i) = MAX(stencil_depths(i), spectral_stencil_extent + 1)
       end if
     end do
 
